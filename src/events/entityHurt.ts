d7f0f140739b6cfb8362fda748b62c2f771f8fd7
Link <50463727+link-discord@users.noreply.github.com>
2023-10-20 22:29:46 +0200

    Projectile Detection Fix
    Did an oopsie

--- src/events/entityHurt.ts
+++ src/events/entityHurt.ts
@@ -1,27 +1,20 @@
 import type { Event } from '../types/Event'
-import type { Entity } from 'prismarine-entity'
-import { stop } from '../utils/stop'
 
 const event: Event = {
     name: 'entityHurt',
-    async execute(bot, entity: Entity) {
-        if (
-            entity.type === 'player' &&
-            bot.swordpvp.target &&
-            bot.swordpvp.target.username === entity.username
-        )
-            stop(bot)
+    async execute(bot, entity) {
+        if (entity.type !== 'player' || entity.username !== bot.username) return
 
-        if (entity.type !== 'projectile') return
+        bot.once('entityGone', (entity) => {
+            if (entity.type !== 'projectile') return
 
-        const projectile = bot.state.shotProjectiles.get(entity.id)
+            const projectile = bot.state.shotProjectiles.get(entity.id)
 
-        if (!projectile) return
+            if (!projectile) return
 
-        await Bun.sleep(50)
-
-        bot.emit('botAttacked', projectile.attacker)
-        bot.state.shotProjectiles.delete(entity.id)
+            bot.emit('botAttacked', projectile.attacker)
+            bot.state.shotProjectiles.delete(entity.id)
+        })
     }
 }
 
