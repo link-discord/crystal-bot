b1a1716eaaaf669f8f6b5fcec79cbbd2128b4b71
Link <50463727+link-discord@users.noreply.github.com>
2023-10-20 23:55:32 +0200

    Tridents also get detected now (again)

--- src/events/entityHurt.ts
+++ src/events/entityHurt.ts
@@ -4,7 +4,6 @@ import type { Event } from '../types/Event'
 const event: Event = {
     name: 'entityHurt',
     async execute(bot, hurtEntity: Entity) {
-
         if (hurtEntity.type !== 'player') return
 
         bot.once('entityGone', (entity) => {
@@ -19,6 +18,29 @@ const event: Event = {
             bot.emit('playerAttacked', hurtEntity, projectile.attacker)
             bot.state.shotProjectiles.delete(entity.id)
         })
+
+        // check if there is a projectile nearby and sort them by distance
+        const nearbyProjectiles = Object.values(bot.entities)
+            .filter((entity) => entity.type === 'projectile')
+            .filter((entity) => entity.position.distanceTo(hurtEntity.position) < 3.5)
+            .sort(
+                (a, b) => {
+                    const distanceA = a.position.distanceTo(hurtEntity.position)
+                    const distanceB = b.position.distanceTo(hurtEntity.position)
+
+                    return distanceA - distanceB
+                }
+            )
+
+        if (nearbyProjectiles.length) {
+            const projectile = nearbyProjectiles[0]
+            const projectileInfo = bot.state.shotProjectiles.get(projectile.id)
+
+            if (!projectileInfo) return
+
+            bot.emit('playerAttacked', hurtEntity, projectileInfo.attacker)
+            bot.state.shotProjectiles.delete(projectile.id)
+        }
     }
 }
 
