327be3854b9f47e05f6b08511bf10dfff08e0370
Link <50463727+link-discord@users.noreply.github.com>
2023-10-20 18:31:53 +0200

    Extra stuff

--- src/commands/equip.ts
+++ src/commands/equip.ts
@@ -1,11 +1,31 @@
 import type { Command } from '../types/Command'
+import type { EquipmentDestination } from 'mineflayer'
+
+const EquipmentDestinations = ['hand', 'head', 'torso', 'legs', 'feet', 'off-hand']
 
 const command: Command = {
     name: 'equip',
     execute(bot, username, args) {
-        if (args[0] === 'armor') {
+        if (!args[0] || args[0] === 'armor') {
             // @ts-ignore
             bot.armorManager.equipAll()
+        } else if (args[0]) {
+            const item = bot.inventory.items().find((item) => item.name === args[0])
+
+            if (!item) {
+                bot.chat('Item not found.')
+                return
+            }
+
+            // check if args[1] is a valid EquipmentDestination
+
+            if (args[1] && !EquipmentDestinations.includes(args[1])) {
+                bot.chat('Invalid equipment destination.')
+                return
+            }
+
+            if (args[1]) void bot.equip(item, args[1] as EquipmentDestination)
+            else void bot.equip(item, 'hand')
         }
     }
 }
